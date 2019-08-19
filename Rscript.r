1411446ff5fd057b2125869720c000a52e0da7f1
Sudhanshu Mohan <sk22.3.1999@live.in>
2019-08-20 01:43:18 +0530

    Update Rscript.r

--- Rscript.r
+++ Rscript.r
@@ -3,10 +3,19 @@ library(plotly) # Interactive data visualizations
 library(psych) # Will be used for correlation visualizations
 library(rattle) # Graphing decision trees
 library(caret) # Machine learning
+library(RColorBrewer) #for colors?
 
-data <- read.csv("~/GitHub/DSc-Project/Datasets Available/india-air-quality-data/data.csv", header=FALSE)
+data <- read.csv("~/GitHub/DSc-Project/Datasets Available/india-air-quality-data/data.csv", header=TRUE)
+
 View(data)
+#head(data$) #6 rows
+#scatter.smooth(data$location, data$type)
 
-head(data) #6 rows
-#scatter.smooth(data)
-plot(data)
+?density
+
+plot(data$location_monitoring_station,type="o")
+plot(density(data$rspm,na.rm = TRUE))
+plot(density(data$so2,na.rm = TRUE))
+plot(density(data$no2,na.rm = TRUE))
+plot(density(data$pm2_5,na.rm = TRUE))
+
